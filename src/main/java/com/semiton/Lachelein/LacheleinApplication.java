ff679b6a1febe28f0f1c522c6a20844b3948d7bf
coldgeon <cksrjs4726@gmail.com>
2025-08-02 19:06:55 +0900

    [Init] project init

---
+++ src/main/java/com/semiton/Lachelein/LacheleinApplication.java
@@ -0,0 +1,13 @@
+package com.semiton.Lachelein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacheleinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacheleinApplication.class, args);
+	}
+
+}
